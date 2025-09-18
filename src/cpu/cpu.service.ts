d28573bd01779be90ac11119484e1ee06673a539
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-18 16:42:00 +0700

    1.4 menjalankan banyak module dalam 1 controller

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -3,11 +3,11 @@ import { PowerService } from 'src/power/power.service';
 
 @Injectable()
 export class CpuService {
-    constructor(private powerService: PowerService){}
+    constructor(private powerService: PowerService){};
 
     compute(a: number, b: number){
         console.log('Cpu membutuhkan 10 watt untuk menghitung');
-        this.powerService.supplyPower(10,'Cpu')
+        this.powerService.supplyPower(10,'Cpu');
         return a + b;   
     }
 }
